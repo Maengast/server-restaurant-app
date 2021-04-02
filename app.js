76b0f0d293f69d817b0426768f94ef4be59c6979
unknown <adntouch@gmail.com>
2021-04-02 12:39:33 +0200

    enable cors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const restaurantRouter = require('./routes/restaurants');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use('/', restaurantRouter);
 
 module.exports = app;
