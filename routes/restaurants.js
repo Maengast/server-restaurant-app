65e79ddd53bdbce98a771e3effd23d2589ca614f
unknown <adntouch@gmail.com>
2021-04-02 13:22:07 +0200

    fix res

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,24 +6,24 @@ const api = require('./restaurant-api')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   api.get(req.query).then((result) => {
-    console.log(res)
-    result.data.pipe(res)
+    console.log(result.data)
+    res.json(result.data.businesses)
   }).catch((e) => {
     const error = e.response
-    console.log(error.data.error)
+    console.log(e)
     res.status(error.status).send(error.data.error)
   })
 });
 
-router.get('/:id', function(req, res, next) {
-  api.getRestaurant(req.params.id).then((result) => {
-    console.log(res)
-    result.data.pipe(res)
-  }).catch((e) =>{
-    const error = e.response
-    console.log(error.data.error)
-    res.status(error.status).send(error.data.error)
-  })
-});
+// router.get('/:id', function(req, res, next) {
+//   api.getRestaurant(req.params.id).then((result) => {
+//     console.log(res)
+//     result.data.pipe(res)
+//   }).catch((e) =>{
+//     const error = e.response
+//     console.log(error.data.error)
+//     res.status(error.status).send(error.data.error)
+//   })
+// });
 
 module.exports = router;
