509b97043e32ec5e967396590e138ecdb3aa0bc7
unknown <adntouch@gmail.com>
2021-04-03 10:28:27 +0200

    fix details

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -15,15 +15,15 @@ router.get('/', function(req, res, next) {
   })
 });
 
-// router.get('/:id', function(req, res, next) {
-//   api.getRestaurant(req.params.id).then((result) => {
-//     console.log(res)
-//     result.data.pipe(res)
-//   }).catch((e) =>{
-//     const error = e.response
-//     console.log(error.data.error)
-//     res.status(error.status).send(error.data.error)
-//   })
-// });
+router.get('/:id', function(req, res, next) {
+  api.getRestaurant(req.params.id).then((result) => {
+    console.log(result.data)
+    res.json(result.data)
+  }).catch((e) =>{
+    const error = e.response
+    console.log(error.data.error)
+    res.status(error.status).send(error.data.error)
+  })
+});
 
 module.exports = router;
