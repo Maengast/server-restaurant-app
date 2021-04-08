d069f775e1878dbf5c7f85705d1322fc23af6abe
unknown <adntouch@gmail.com>
2021-04-08 18:20:53 +0200

    fix timeout

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 const api_key = "Ai8f_CNF-7pJC0Nthi-UcLFtfvqKBY-9nJ96K5zAf9sViJTRoJlZOwiOVH7kOM74W6H02iyRGMvqddwm537QigXyL-isElHkq2ipDSYkIxM3nvIjUBTXA8mzhmNcYHYx"
 const instance = axios.create({
     baseURL: 'https://api.yelp.com/v3/',
-    timeout: 1000,
     headers: {'Authorization': 'Bearer ' + api_key}
 })
 
